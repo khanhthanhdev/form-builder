7859f932dccf9a3e008ea5e860be98b52ad282b3
khanhthanhdev <thanhkt27507@gmail.com>
2023-12-24 12:03:34 +0700

    Add preview,publish button

--- components/PreviewDialogBtn.tsx
+++ components/PreviewDialogBtn.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from './ui/button'
+import {MdPreview} from "react-icons/md"
 
 const PreviewDialogBtn = () => {
-  return (
-    <div>PreviewDialogBtn</div>
-  )
+  return <Button variant={"outline"} className='gap-2'>
+    <MdPreview className="h-6 w-6" />
+    Preview
+  </Button>
 }
 
 export default PreviewDialogBtn
